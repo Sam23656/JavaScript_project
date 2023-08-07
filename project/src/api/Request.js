53b4663900c1f8cce98c202860fda52e009e99a8
Sam23656 <sem26m06@gmail.com>
2023-08-07 16:31:07 +0500

    Add Playlists (full)

--- project/src/api/Request.js
+++ project/src/api/Request.js
@@ -3,7 +3,7 @@ async function Request(url){
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '6d0575a6c3msh73478090d40e2b7p1312d0jsn9cdcbc3eff9f',
+            'X-RapidAPI-Key': 'e4465e092bmshaeaba5630527e13p120ee9jsnf2b101068185',
             'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
         }
     };
--- project/src/components/Playlist.jsx
+++ project/src/components/Playlist.jsx
@@ -10,15 +10,23 @@ function Playlist(){
     const searchParams = new URLSearchParams(location.search);
     const uriString = searchParams.get('uri');
     const uri = JSON.parse(decodeURIComponent(uriString));
-    const [album, setAlbum] = useState(null);
-    const [SoundAlbum, setSoundAlbum] = useState("");
+    const [playlist, setplaylist] = useState(null);
+    const [playlistSongs, setplaylistSongs] = useState(null);
+    const [Soundplaylist, setSoundplaylist] = useState("");
   
     async function add_search_results() {
       const url = `https://spotify23.p.rapidapi.com/playlist/?id=${uri.uri}`;
       const data = await Request(url);
-      setAlbum(data);
+      setplaylist(data);
+      add_search_results_tracks()
     }
     
+    async function add_search_results_tracks() {
+        const url = `https://spotify23.p.rapidapi.com/playlist_tracks/?id=${uri.uri}&offset=0&limit=100`;
+        const data = await Request(url);
+        setplaylistSongs(data.items);
+      }
+      
     function msToMAndS(ms) {
       const minutes = Math.floor(ms / 60000); 
       const seconds = ((ms % 60000) / 1000).toFixed(0);
@@ -26,8 +34,57 @@ function Playlist(){
     }
     
     function play_sound(uri) {
-      GetSound(uri, setSoundAlbum);
+        if (Soundplaylist !== null) {
+            GetSound(uri, setSoundplaylist);
+          }
+      GetSound(uri, setSoundplaylist);
     } 
+    useEffect(() => {
+        if (playlist == null || playlistSongs == null) {
+          add_search_results();
+        }
+      }, []);
+      return (
+        <>
+          {playlistSongs ? (
+            <>
+              <header className='header'>
+                <img className='img' src={playlist.images[1].url} alt="" />
+                <div className='names'>
+                  <h3 className='name'>{playlist.name}</h3>
+                  <div className='user'>
+                        <p>{playlist.owner.display_name}</p>
+                  </div>
+                </div>
+              </header>
+              
+              <main className='main2'>
+                <div className='tracks'>
+                {playlistSongs.map((elem) => (
+                    <div onClick={() => play_sound(elem.track.id)} key={elem.track.id} className='track'>
+                      <div className='track-name-time'>
+                        <p>{elem.track.name}</p>
+                        <p className='time'>{msToMAndS(elem.track.duration_ms)}</p>
+                      </div>
+                      <div className='track-artists'>
+                        {elem.track.artists.map((artist) => (
+                          <p key={artist.id} className='track-artist'>{artist.name}</p>
+                        ))}
+                      </div>
+                    </div>
+                  ))} 
+                </div>
+              </main>
+          
+              <footer>
+                <AudioPlayer Sound={Soundplaylist} />
+              </footer>
+            </>
+          ) : (
+            <div>Loading...</div>
+          )}
+        </>
+      );
     
 }
 
